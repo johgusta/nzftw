55639df954c5bcf20fb4d717d7ad5d85ea9f612c
Johan Gustafson <johan_gustafson@datawatch.com>
2016-12-06 13:55:51 +0100

    Add basic phaser game and static assets folder

--- src/js/game.js
+++ src/js/game.js
@@ -1,15 +1,31 @@
 require('./game.css');
 
+window.PIXI = require('phaser/build/custom/pixi');
+window.p2 = require('phaser/build/custom/p2');
+window.Phaser = require('phaser/build/custom/phaser-split');
+
 function Game(gameContainer) {
 
-    var canvas = document.createElement('canvas');
-    canvas.classList.add('game-canvas');
-    canvas.width = 800;
-    canvas.height = 600;
-    gameContainer.appendChild(canvas);
+    var phaserDiv = document.createElement('div');
+    phaserDiv.id = 'phaser-container';
+    phaserDiv.classList.add('game-canvas');
+    gameContainer.appendChild(phaserDiv);
 
-    var context = canvas.getContext('2d');
-    context.fillText('Hello world', 10, 10);
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-container', { preload: preload, create: create, update: update });
+
+    function preload() {
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('ground', 'assets/platform.png');
+        game.load.image('star', 'assets/star.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    }
+
+    function create() {
+        game.add.sprite(0, 0, 'star');
+    }
+
+    function update() {
+    }
 }
 
 module.exports = Game;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
-var path = require("path");
+var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: {
-        app: ["./src/js/app.js"]
+        app: ['./src/js/app.js']
     },
     output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: 'style!css' }
         ]
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './src/static' }
+        ])
+    ]
 };
